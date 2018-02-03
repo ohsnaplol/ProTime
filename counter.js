a54395b687085719590c046fc6b6d91c3125e1cf
tlneondo <tlneondo@gmail.com>
2018-02-03 18:22:31 -0500

    added counter.js

---
+++ counter.js
@@ -0,0 +1,13 @@
+function dumbCountUpTimer(){
+var seconds = 0;
+var funksec = setInterval(function(){seconds++}, 1000);
+
+console.log(seconds);
+console.log('funksec= '+ funksec);
+
+
+return time;
+
+}
+
+

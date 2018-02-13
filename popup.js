f3e06a6af2892b13f8746eb7807a7c3ee5edada6
ohsnaplol <emyb@bellsouth.net>
2018-02-13 12:12:27 -0500

    removed unnecessary elements

--- popup.js
+++ popup.js
@@ -1,33 +1,4 @@
 setInterval(waterFilter, 2000)
-// // first, check if they have previously check marked this box.
-// if (localStorage.getItem('isEnabled') === null) {
-//   localStorage.setItem('isEnabled', true)
-//   $('#checkBox').prop('checked', true)
-//   setInterval(waterFilter, 2000)
-// }
-//
-// $(function() {
-//   console.log('READY!')
-// })
-//
-// if (localStorage.getItem('isEnabled')) {
-//   $('#checkBox').prop('checked', true)
-//   setInterval(waterFilter, 2000)
-// } else {
-//   $('#checkBox').prop('checked', false)
-// }
-//
-// // If they check mark, isEnabled set to true, remember setting,
-// // else, if they deselect checkmark, isEnabled- set to false and remember setting
-// $('#checkBox').on('click', function() {
-//   console.log($(this).is(':checked'))
-//   if ($(this).is(':checked')) {
-//     localStorage.setItem('isEnabled', true)
-//     setInterval(waterFilter, 2000)
-//   } else {
-//     localStorage.setItem('isEnabled', false)
-//   }
-// })
 
 function waterFilter() {
   var images = document.getElementsByTagName('img')
